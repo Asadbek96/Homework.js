61a0c3886bd2c2adf8dab778a728210dd2a32a15
Asadbek <asadbekesemuratov2@gmail.com>
2024-10-30 23:47:47 +0500

    HW3-WEEK9-JS

---
+++ HW-3-week-9-js/js/script.js
@@ -0,0 +1,63 @@
+// var name = "Satybaldy"
+
+// var sum = null
+// console.log('Sum:' + sum);
+
+// var san1 = 10
+// var san2 = 15
+
+// sum = san1 + san2
+// console.log('Sum:', sum);
+
+// if (true) {
+//     var name = 'Galymbek'
+//     console.log('шекара ішіндегі аты' + name);
+
+//     if (true) {
+//         var name = 'Nurs'
+//         console.log('шекара ішіндегі аты' + name);
+        
+//     }
+    
+// }
+// console.log('сыртқы аты' + name);
+
+
+// var number1 = 25
+// console.log('number1 type: ' + typeof number1);
+// console.log(number1);
+
+// var number2 = number1.toString()
+// console.log('number type: ' + typeof number2);
+// console.log(number2);
+
+// var sum1 =null;
+
+// sum1 = number1 + number2
+
+// console.log(sum1);
+
+
+let name = 'Asadbek';
+console.log(name);
+console.log('name type: ' + typeof name);
+
+
+var age = 20;
+var age = age.toString();
+console.log(age);
+console.log('age type: ' + typeof age);
+
+
+var isStudent = false;
+isStudent = !isStudent;
+console.log('Is he a student?:' +  isStudent )
+console.log('isStudent type: ' + typeof isStudent);
+
+
+
+
+
+
+
+
