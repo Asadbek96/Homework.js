0e8c207fda02685b2f3bd8c78ee947f000b85180
Asadbek <asadbekesemuratov2@gmail.com>
2024-10-31 23:10:54 +0500

    HW4-WEEK9-JS

--- HW-3-week-9-js/js/script.js
+++ HW-3-week-9-js/js/script.js
@@ -1,18 +1,21 @@
-let name = 'Asadbek';
-console.log(name);
-console.log('name type: ' + typeof name);
+// let name = 'Asadbek';
+// console.log(name);
+// console.log('name type: ' + typeof name);
 
 
-var age = 20;
-var age = age.toString();
-console.log(age);
-console.log('age type: ' + typeof age);
+// var age = 20;
+// var age = age.toString();
+// console.log(age);
+// console.log('age type: ' + typeof age);
 
 
-var isStudent = false;
-isStudent = !isStudent;
-console.log('Is he a student?:' +  isStudent )
-console.log('isStudent type: ' + typeof isStudent);
+// var isStudent = false;
+// isStudent = !isStudent;
+// console.log('Is he a student?:' +  isStudent )
+// console.log('isStudent type: ' + typeof isStudent);
+
+
+
 
 
 
---
+++ HW4-WEEK9-JS/js/script.js
@@ -0,0 +1,5 @@
+var num1 = Number(prompt('Number 1'))
+var num2 = Number(prompt('Number 2'))
+var num3 = Number(prompt('Number 3'))
+
+alert('Орташа мәні:' + (num1 + num2 + num3) / 3);
