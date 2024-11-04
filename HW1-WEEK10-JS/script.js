98825472bd050c638549241552de628ea292cceb
Asadbek <asadbekesemuratov2@gmail.com>
2024-11-04 22:30:04 +0500

    HW1-WEEK10-JS

---
+++ HW1-WEEK10-JS/script.js
@@ -0,0 +1,18 @@
+//TASK-1
+let age = 60
+let isStudent = true
+
+alert((age>=65 || isStudent))
+
+
+//TASK-2
+let num = Number(prompt('Write a number:'))
+alert(num>=10 && num<=20)
+
+
+//TASK-3
+let num1 = Number(prompt('Write a number1:'))
+let num2 = Number(prompt('Write a number2:'))
+
+alert((num1<=0 && num2>=0) || (num1>=0 && num2<=0))
+
