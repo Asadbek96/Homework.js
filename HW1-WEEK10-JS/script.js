3f32045b6f9cd97240513c392f3def2193326019
Asadbek <asadbekesemuratov2@gmail.com>
2024-11-05 23:11:15 +0500

    HW2-WEEK10-JS

--- HW1-WEEK10-JS/script.js
+++ HW1-WEEK10-JS/script.js
@@ -1,18 +1,27 @@
 //TASK-1
-let age = 60
-let isStudent = true
+// let age = prompt("Жасыңызды енгізіңіз");
+// let isStudent = true;
 
-alert((age>=65 || isStudent))
+// alert((age>=65 || isStudent == true))
 
 
 //TASK-2
-let num = Number(prompt('Write a number:'))
-alert(num>=10 && num<=20)
+// let num = Number(prompt('Write a number:'))
+// alert(num>=10 && num<=20)
 
 
 //TASK-3
-let num1 = Number(prompt('Write a number1:'))
-let num2 = Number(prompt('Write a number2:'))
+// let num1 = +prompt('Write a first number:')
+// let num2 = +prompt('Write a second number:')
 
-alert((num1<=0 && num2>=0) || (num1>=0 && num2<=0))
+// alert((num1<=0 && num2>=0) || (num1>=0)) 
+
+// let num = +prompt('Write a number:')
+
+// if(num % 2 == 0) {
+//     alert("Жұп сан")
+// }
+// else{
+//     alert("Тақ сан")
+// }
 
