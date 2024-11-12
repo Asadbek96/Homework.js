7fcb9703088a286ccd2fbc3ff0066d8459eae749
Asadbek <asadbekesemuratov2@gmail.com>
2024-11-13 00:15:21 +0500

    HW2-WEEK11-JS

--- HW2-WEEK11-JS/script.js
+++ HW2-WEEK11-JS/script.js
@@ -5,7 +5,7 @@ for (let i = 1; i <= san; i++) {
     console.log('3 ke  jane 5 ke bolinetin san:' + i)
     if (i % 3 == 0 && i % 5 == 0 && i % 2 == 0) {
       console.log(i + ' - Sany 3 ke  jane 5 ke bolinedi jane jup san. BREAK!!!')
-      break
+      break;
     }
   }
 }
