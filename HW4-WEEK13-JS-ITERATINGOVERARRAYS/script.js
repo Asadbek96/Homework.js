39d6b1ee785697d7cc40fe40884599efe31b3a38
Asadbek <asadbekesemuratov2@gmail.com>
2024-11-29 21:13:01 +0500

    HW4-WEEK13-JS-ITERATINGOVERARRAYS

---
+++ HW4-WEEK13-JS-ITERATINGOVERARRAYS/script.js
@@ -0,0 +1,28 @@
+//TASK-1
+// let numbers = [1,2,3,4,5]
+// let kubSandar = numbers.map(num => num * num * num)
+// let taqSandar = []
+// for(let num of kubSandar){
+//     if (num % 2 != 0) {
+//       taqSandar.push(num)
+//       console.log('Kubtalgan sandar ishindegi taq san: ' + num)
+//     }
+// }
+
+//TASK-2
+// let studentSany = +prompt('Student sanyn engiziniz')
+// let bagalary = []
+// let sum = 0
+// let ortaqBaga = 0
+// let jogaryBaga = []
+// for(let i = 1; i <= studentSany; i++){
+//     let studentBagasy = +prompt(i + ' - Studenttin bagasy')
+//     bagalary.push(studentBagasy)
+//     ortaqBaga += studentBagasy / studentSany
+//     if(studentBagasy>=85){
+//         jogaryBaga.push(studentBagasy)
+//     }
+// }
+// console.log('Engizizlgen bagalar: ' + bagalary);
+// console.log('Ortaq baga: ' + ortaqBaga);
+// console.log('85-ten jogary bagalar: ' + jogaryBaga);
